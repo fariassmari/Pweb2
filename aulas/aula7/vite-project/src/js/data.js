2c729d2cd124ba6473df43a92f193ce2d297476e
Mariana Farias <marianafariasbrito@gmail.com>
2025-10-31 20:09:47 +0000

    Aula 7

---
+++ aulas/aula7/vite-project/src/js/data.js
@@ -0,0 +1,29 @@
+export const investments = [
+    {
+        id: 1,
+        name: "Tesouro Selic 2029",
+        value: 100.50,
+        origin: 'Tesouro Nacional',
+        category: 'Pos',
+        date: '2023-03-22',
+        interest: '100% Selic'
+    },
+    {
+        id: 2,
+        name: 'Tesouro IPCA 2029',
+        value: 100,
+        origin: 'Tesouro Nacional',
+        category: 'IPCA',
+        date: '2023-03-22',
+        interest: 'IPCA + 5.83%'
+    },
+    {
+        id: 3,
+        name: 'Tesouro IPCA 2031',
+        value: 1000,
+        origin: 'Tesouro Nacional',
+        category: 'IPCA',
+        date: '2023-03-22',
+        interest: 'IPCA + 7.83%'
+    },
+];
